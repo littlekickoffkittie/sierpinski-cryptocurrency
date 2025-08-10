873b459eac28e8fb5a0bd60d59a24d7dd50738a6
littlekickoffkittie <financingcanfunk@gmail.com>
2025-08-10 04:31:10 -0400

    Add src/network.rs 1754814669917

---
+++ src/network.rs
@@ -0,0 +1,35 @@
+use crate::transaction::Transaction;
+
+pub struct Network {
+    pub transactions: Vec<Transaction>,
+}
+
+impl Network {
+    pub fn new() -> Self {
+        Self {
+            transactions: Vec::new(),
+        }
+    }
+
+    pub fn broadcast(&mut self, transaction: Transaction) {
+        self.transactions.push(transaction);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::transaction::{Transaction, TransactionType};
+    use crate::triangle::Triangle;
+
+    #[test]
+    fn test_broadcast() {
+        let mut network = Network::new();
+        let triangle = Triangle::genesis();
+        let transaction = Transaction::new(triangle.id.clone(), TransactionType::Create);
+
+        network.broadcast(transaction);
+
+        assert_eq!(network.transactions.len(), 1);
+    }
+}
